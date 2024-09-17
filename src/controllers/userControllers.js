a9274dcd551e471a480a7b982f5642f06f16945e
pjroggy <pierrejeanroggy@gmail.com>
2024-09-17 16:32:25 +0200

    express quest 01.2

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,34 @@
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
